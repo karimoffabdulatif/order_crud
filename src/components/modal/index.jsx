768745d0ac254415a7d609163ef2731867009860
karimoffabdulatif <akarimoff001@gmail.com>
2024-07-06 01:56:21 +0500

    first commit

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,7 @@
+import LogOutModal from "./logout";
+import VerifyModal from "./verify";
+import SignInModal from "./signin";
+import Service from "./service";
+import Order from "./order";
+
+export { LogOutModal, VerifyModal, SignInModal, Service, Order };
